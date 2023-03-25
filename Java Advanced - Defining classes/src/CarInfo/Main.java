3f854b6635f851454f47ec274dbcef706b4dfc17
Ventsislav Kisyov <ventsislav755@gmail.com>
2023-03-25 10:46:09 +0200

    exercise wuth classes

---
+++ Java Advanced - Defining classes/src/CarInfo/Main.java
@@ -0,0 +1,23 @@
+package CarInfo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Car car = new Car();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for(int i=0; i<n; i++){
+            String[] arr = scanner.nextLine().split(" ");
+            car.setBrand(arr[0]);
+            car.setModel(arr[1]);
+            car.setHorsePower(Integer.parseInt(arr[2]));
+
+            System.out.println(car.carInfo());
+        }
+    }
+}
